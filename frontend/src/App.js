a4a1fd2def51f525439e20f84f118eabe87ffcdc
Kevin Black <kwb201@gmail.com>
2021-06-11 10:51:19 -0400

    first api call to unsplash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 
 
@@ -11,8 +11,16 @@ function App() {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
-    e.preventDefault()
-    console.log(word)
+    e.preventDefault();
+    console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 
   
